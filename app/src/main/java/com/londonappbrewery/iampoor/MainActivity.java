74f192d6dee48710e8ba1867815bef205b9a6019
Vinicius <vinians2006@yahoo.com.br>
2017-11-29 19:47:54 -0200

    Updated today!

--- app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/iampoor/MainActivity.java
@@ -3,12 +3,21 @@ package com.londonappbrewery.iampoor;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private int numberOfClicks = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onClick(View view) {
+        numberOfClicks++;
+        Button btn = (Button) view;
+        btn.setText("You clicked " + numberOfClicks + " time(s)");
     }
 }
